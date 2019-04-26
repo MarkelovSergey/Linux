e032da9000437f3cd3d7386f341b747cd8f812c7
MarkelovSergey <kerzhakov.08@mail.ru>
2019-04-26 20:56:01 +0500

    Update unistall.php

--- unistall.php
+++ unistall.php
@@ -2,4 +2,28 @@
 
 unlink( __DIR__ . '/shell.sh');
 unlink( __DIR__ . '/conf.php');
-unlink( __DIR__ . '/unistall.php');
+//unlink( __DIR__ . '/unistall.php');
+unlink( __DIR__ . '/../master.zip' );
+Delete( __DIR__ . '/../Linux-master' );
+
+function Delete($path)
+{
+    if (is_dir($path) === true)
+    {
+        $files = array_diff(scandir($path), array('.', '..'));
+
+        foreach ($files as $file)
+        {
+            Delete(realpath($path) . '/' . $file);
+        }
+
+        return rmdir($path);
+    }
+
+    else if (is_file($path) === true)
+    {
+        return unlink($path);
+    }
+
+    return false;
+}
