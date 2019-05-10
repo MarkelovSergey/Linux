6c9e41b2c4a8abbb91fc204a5eae759df5261d14
MarkelovSergey <kerzhakov.08@mail.ru>
2019-05-10 09:49:13 +0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ apt update
 apt upgrade -y
 apt install nginx nano -y
 apt install mysql-server -y
-apt install php-fpm php-mysql -y
+apt install php7.2-fpm php7.2-mysql -y
 php conf.php
 systemctl restart php7.2-fpm.service
 php unistall.php
