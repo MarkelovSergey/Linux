1704c9904086719fa1c3932728a0e1a92dadaf64
MarkelovSergey <kerzhakov.08@mail.ru>
2019-05-13 13:22:28 +0500

    Update conf.php

--- wordpress/conf.php
+++ wordpress/conf.php
@@ -11,7 +11,7 @@ fclose($fd);
 $pathPhpConf = '/etc/php/7.2/fpm/pool.d/www.conf';
 
 $phpConf =  file_get_contents( $pathPhpConf );
-$newPhpConf = str_replace('user = www-data', 'user = wpuser', $nginxConf);
+$newPhpConf = str_replace('user = www-data', 'user = wpuser', $phpConf);
 $fd = fopen( $pathPhpConf, 'w') or die("не удалось создать файл");
 fwrite($fd, $newPhpConf);
 fclose($fd);
