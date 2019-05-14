5b96ab4daaf92223e7fa6cebdb203e4f6637f176
MarkelovSergey <kerzhakov.08@mail.ru>
2019-05-14 16:37:15 +0500

    Create redis-install.sh

---
+++ wordpress/redis-install.sh
@@ -0,0 +1,8 @@
+apt install redis-server php-redis -y
+systemctl start redis.service
+systemctl enable redis.service
+
+cd /var/www/html
+su - wpuser -c "wp plugin install redis-cache"
+su - wpuser -c "wp plugin activate  redis-cache"
+su - wpuser -c "wp redis enable"
