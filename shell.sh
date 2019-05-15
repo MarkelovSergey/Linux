17fb5dfd6cbbfb1abeceb053ed2c7c9e406f65e9
MarkelovSergey <kerzhakov.08@mail.ru>
2019-05-15 10:16:03 +0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,9 +2,6 @@
 
 apt update
 apt upgrade -y
-apt install nginx nano -y
-apt install mysql-server -y
-apt install php7.2-fpm php7.2-mysql -y
-php conf.php
+apt install nano nginx mysql-server php7.2-fpm php7.2-mysql -y
+sed -i 's/.*;cgi.fix_pathinfo=1.*/cgi.fix_pathinfo=0/' /etc/php/7.2/fpm/php.ini
 systemctl restart php7.2-fpm.service
-#php unistall.php
