21b0ff5f6ac88196d58d48efd074e039e5983c38
MarkelovSergey <kerzhakov.08@mail.ru>
2019-05-15 10:37:06 +0500

    Update install.sh

--- wordpress/install.sh
+++ wordpress/install.sh
@@ -6,28 +6,32 @@ mysql -e "GRANT ALL PRIVILEGES ON wordpress . * TO 'wordpressuser'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
 
 apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
-systemctl restart php7.2-fpm
+#systemctl restart php7.2-fpm
 
 mv default /etc/nginx/sites-available/default
-systemctl restart nginx
+#systemctl restart nginx
 
+#установка wp-cli
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+#настройка прав
 useradd -d /var/www/html wpuser -p 4680
 usermod -aG www-data wpuser
 chown -R wpuser:www-data /var/www
 chown -R wpuser:www-data /var/lib/nginx
 chown -R wpuser:www-data /var/log/nginx
 chown -R wpuser:www-data /var/lib/php/sessions
-#php conf.php
+
+#меняет юзера для nginx, php-fpm
 sed -i 's/.*user www-data;.*/user wpuser;/' /etc/nginx/nginx.conf
 sed -i 's/.*user = www-data.*/user = wpuser/' /etc/php/7.2/fpm/pool.d/www.conf
 
 service php7.2-fpm restart
 service nginx restart
 
+#wp-cli устанавливает wordpress, отключает дефолтные плагины
 cd /var/www/html
 su - wpuser -c "wp core download --locale=ru_RU"
 su - wpuser -c "wp config create --dbname=wordpress --dbuser=wordpressuser --dbpass=4680 --locale=ru_RU"
